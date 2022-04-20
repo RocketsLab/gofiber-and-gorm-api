7eb6ce5d524b185d75246911cd83c9d7ed4d3816
jjsquady <jorge.techinfo@gmail.com>
2022-04-20 03:07:20 -0300

    rename router

--- routes/web.go
+++ routes/web.go
@@ -6,15 +6,18 @@ import (
 )
 
 func RegisterWebRoutes() {
+
+	router := application.App
+
 	//USERS
 	userController := controllers.UserController{}
-	application.App.Get("/users", userController.Index)
-	application.App.Post("/users", userController.Store)
-	application.App.Get("/users/:user", userController.Show)
+	router.Get("/users", userController.Index)
+	router.Post("/users", userController.Store)
+	router.Get("/users/:user", userController.Show)
 
 	//AUTH
 	authController := controllers.AuthController{}
-	application.App.Post("/auth/login", authController.Login).Name("auth.login")
-	application.App.Post("/auth/logout", authController.Logout).Name("auth.logout")
-	application.App.Post("/auth/register", authController.Register).Name("auth.register")
+	router.Post("/auth/login", authController.Login).Name("auth.login")
+	router.Post("/auth/logout", authController.Logout).Name("auth.logout")
+	router.Post("/auth/register", authController.Register).Name("auth.register")
 }
